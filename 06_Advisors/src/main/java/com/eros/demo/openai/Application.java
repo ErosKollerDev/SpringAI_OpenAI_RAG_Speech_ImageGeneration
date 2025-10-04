a4eda636f418cadab1f0a6d3bc43ab4aa7119a9f
ErosKollerDev <eroskoller.dev@gmail.com>
2025-10-04 15:24:34 -0300

    Adding h2 jdbc for Chat memory

--- 06_Advisors/src/main/java/com/eros/demo/openai/Application.java
+++ 06_Advisors/src/main/java/com/eros/demo/openai/Application.java
@@ -1,9 +1,11 @@
 package com.eros.demo.openai;
 
+import org.springframework.ai.model.chat.memory.autoconfigure.ChatMemoryAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class Application {
 
     public static void main(String[] args) {
