d84aa5ae79398dbfc122a0d2eef04f5e49eb31fb
ErosKollerDev <eroskoller.dev@gmail.com>
2025-10-08 17:19:14 -0300

    Unit Test with AI (Evaluator)

---
+++ 09_unit_test/src/test/java/com/eazybytes/springai/SpringaiApplicationTests.java
@@ -0,0 +1,29 @@
+package com.eazybytes.springai;
+
+import com.eazybytes.springai.controller.ChatController;
+import org.junit.jupiter.api.*;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.evaluation.FactCheckingEvaluator;
+import org.springframework.ai.chat.evaluation.RelevancyEvaluator;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.evaluation.EvaluationRequest;
+import org.springframework.ai.evaluation.EvaluationResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.test.context.TestPropertySource;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class SpringaiApplicationTests {
+
+
+}
